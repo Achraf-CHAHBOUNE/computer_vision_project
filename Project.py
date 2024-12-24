d8b6e7c47bcbbbf488776a68e48b3c260ddcc803
Achraf Chahboune <achrafchahbon5@gmail.com>
2024-12-24 21:07:56 +0100

    vision

--- Project.py
+++ Project.py
@@ -5,9 +5,13 @@ Check the README.md for complete documentation.
 
 import cv2
 from gaze_tracking import GazeTracking
+from pynput.keyboard import Controller, Key
+
 
 gaze = GazeTracking()
 webcam = cv2.VideoCapture(0)
+
+keyboard = Controller()
 
 while True:
     # We get a new frame from the webcam
@@ -23,10 +27,17 @@ while True:
         text = "Blinking"
     elif gaze.is_right():
         text = "Looking right"
+        keyboard.press(Key.right)
+        keyboard.release(Key.right)
+        print(text)
     elif gaze.is_left():
         text = "Looking left"
+        keyboard.press(Key.left)
+        keyboard.release(Key.left)
+        print(text)
     elif gaze.is_center():
         text = "Looking center"
+        print(text)
 
     cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
 
@@ -35,7 +46,7 @@ while True:
     cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
     cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
 
-    cv2.imshow("Demo", frame)
+    cv2.imshow("3d controller", frame)
 
     if cv2.waitKey(1) == 27:
         break
