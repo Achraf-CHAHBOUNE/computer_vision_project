6bb527180c2461c7c1c2552cca0d5603a832b143
Achraf Chahboune <achrafchahbon5@gmail.com>
2024-12-29 21:02:51 +0100

    new changes in threshold

--- Project.py
+++ Project.py
@@ -1,8 +1,3 @@
-"""
-Demonstration of the GazeTracking library.
-Check the README.md for complete documentation.
-"""
-
 import cv2
 from gaze_tracking import GazeTracking
 from pynput.keyboard import Controller, Key
@@ -14,7 +9,6 @@ webcam = cv2.VideoCapture(0)
 keyboard = Controller()
 
 while True:
-    # We get a new frame from the webcam
     _, frame = webcam.read()
 
     # We send this frame to GazeTracking to analyze it
